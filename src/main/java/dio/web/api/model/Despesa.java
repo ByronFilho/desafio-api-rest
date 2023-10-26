27b1be8f29d2562892810cf626f5e22b05bdd82b
ByronFilho <byronlofilho@gmail.com>
2023-10-26 07:43:49 -0300

    Criação entity

--- src/main/java/dio/web/api/model/Despesa.java
+++ src/main/java/dio/web/api/model/Despesa.java
@@ -1,8 +1,21 @@
 package dio.web.api.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Despesa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(nullable = false)
     private String descricao;
+
     private Double valor;
     
     public Despesa() {
